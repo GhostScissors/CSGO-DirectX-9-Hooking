83879aec33d5b7c232d078d65eb9b91aa9653051
GhostScissors <79089473+GhostScissors@users.noreply.github.com>
2023-07-30 12:59:03 +0530

    Gui class and header files

--- CSGO/src/dllmain.cpp
+++ CSGO/src/dllmain.cpp
@@ -1,6 +1,27 @@
-﻿#include <iostream>
+﻿#define WIN32_LEAN_AND_MEAN
 
-int main()
+#include <Windows.h>
+#include <thread>
+#include <cstdint>
+
+// Setup function
+void Setup(const HMODULE instance)
 {
-    std::cout << "Hello World\n" << std::endl; 
+    
+}
+
+// Entry point
+BOOL WINAPI DllMain(const HMODULE instance, const std::uintptr_t reason, const void* reserved)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(instance);
+
+        const auto thread = CreateThread(nullptr, 0, reinterpret_cast<LPTHREAD_START_ROUTINE>(Setup), instance, 0, nullptr);
+
+        if (thread)
+            CloseHandle(thread);
+    }
+    
+    return TRUE;
 }
--- CSGO/src/gui.h
+++ CSGO/src/gui.h
@@ -1,7 +1,38 @@
 ﻿#pragma once
 
-class gui
+#include <d3d9.h>
+
+namespace gui
 {
-public:
-    
-};
+    // Show menu?
+    inline bool open = true;
+
+    // Is menu setup?
+    inline bool setup = false;
+
+    // Windows Api related
+    inline HWND window = nullptr;
+    inline WNDCLASSEX windowClass = { };
+    inline WNDPROC originalWindowProcess = nullptr;
+
+    // DirectX related
+    inline LPDIRECT3DDEVICE9 device = nullptr;
+    inline LPDIRECT3D9 d3d9 = nullptr;
+
+    bool SetupWindowClass(const char* windowClassName) noexcept;
+    void DestroyWindowClass() noexcept;
+
+    bool SetupWindow(const char* windowName) noexcept;
+    void DestroyWindow() noexcept;
+
+    bool SetupDirectX() noexcept;
+    void DestroyDirectX() noexcept;
+
+    // Setup device
+    void Setup();
+
+    void SetupMenu(LPDIRECT3DDEVICE9 device) noexcept;
+    void Destroy() noexcept;
+
+    void Render() noexcept;
+}
